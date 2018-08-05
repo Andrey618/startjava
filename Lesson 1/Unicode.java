66aa9351ae2b5baf95a723c1bee6a0dafd89a655
Andrey618 <andrey618@gmail.com>
2018-08-05 18:29:20 +0300

    output unicode array {33/ 126}

---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,10 @@
+public class Unicode {	
+	
+	public static void main(String[] args) {
+        for (int i = 33; i <= 126; i++) {
+           	System.out.print ((char) i + ", ");
+        }
+        System.out.println();
+    }
+
+}
