cc7a46c9956e20a3f618d9f20053516eb9386e54
Andrey618 <andrey618@gmail.com>
2018-08-05 11:44:04 +0300

    fixed 9 mistakes - from max

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,32 +1,25 @@
 public class Calculator {	
+	
 	public static void main(String[] args) {
-		int firstVar = 17;
-		int secondVar = 5;
-		double result = 1;
+		int firstNumber = 17;
+		int secondNumber = 5;
 		String sign = "%";
 
-		if (sign=="+"){
-			result = firstVar + secondVar;
-		
-			} else if (sign=="-"){
-				result = firstVar - secondVar;
-				
-				} else if (sign=="*"){
-					result = firstVar * secondVar;
-					
-					}else if (sign=="/"){
-						result = (double)firstVar / secondVar;
-						
-						}else if (sign=="^"){
-							for (int i=1; i<=secondVar;i++){
-								result = result*firstVar;
-								}
-							}else if (sign=="%"){
-								while (firstVar >=secondVar){
-									result = firstVar - secondVar;
-									firstVar = firstVar - secondVar;
-									}
-								}
-		System.out.println(result);
+		if (sign == "+"){
+			System.out.println(firstNumber + secondNumber);
+		} else if (sign == "-"){
+			System.out.println(firstNumber - secondNumber);
+		} else if (sign == "*"){
+			System.out.println(firstNumber * secondNumber);
+		}else if (sign == "/"){
+			System.out.println((double)firstNumber / secondNumber);
+		}else if (sign == "^"){
+			int result = 1;
+			for (int i = 1; i <= secondNumber; i++){
+				result *= firstNumber;
+			}
+		}else if (sign == "%"){
+			System.out.println(firstNumber % secondNumber);
+		}
 	}
 }
