d889f7f8175c3279f5a9147052bebc682775f430
Andrey618 <andrey618@gmail.com>
2018-08-05 18:01:20 +0300

    without break, deleted superfluous code

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -2,21 +2,18 @@ public class MyFirstGame {
 	
 	public static void main(String[] args) {
 		int compNumber = 100;
-		int userNumber = 0;
+		int userNumber = 100;
 	
 		System.out.println("Компьютер загадал число: " + compNumber);				
 		while (userNumber != compNumber) {
-			if (compNumber == userNumber) {
-				System.out.println("Вы угадали! Это число: " + userNumber);
-				break;
-			} else if (userNumber < compNumber) {
+			if (userNumber < compNumber) {
 				System.out.println("Введенное вами число (" + userNumber + ") меньше того, что загадал компьютер");
 				userNumber++;
-			} else if (userNumber > compNumber) {
+			} else {
 				System.out.println("Введенное вами число (" + userNumber + ") больше того, что загадал компьютер");
 				userNumber--;
 			}
 		}
-		System.out.println("Вы угадали! Это число: " + userNumber);	
+		System.out.println("Вы угадали! Это число: " + compNumber);	
 	}
 }
