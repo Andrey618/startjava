84023177dcea87b95c08cabee31f2d6823da3643
Andrey618 <andrey618@gmail.com>
2018-08-05 11:57:09 +0300

    fixed other 3 mistakes from max

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,22 +3,23 @@ public class Calculator {
 	public static void main(String[] args) {
 		int firstNumber = 17;
 		int secondNumber = 5;
-		String sign = "%";
+		String sign = "^";
 
 		if (sign == "+"){
 			System.out.println(firstNumber + secondNumber);
-		} else if (sign == "-"){
+		} else if (sign == "-") {
 			System.out.println(firstNumber - secondNumber);
-		} else if (sign == "*"){
+		} else if (sign == "*") {
 			System.out.println(firstNumber * secondNumber);
-		}else if (sign == "/"){
+		} else if (sign == "/") {
 			System.out.println((double)firstNumber / secondNumber);
-		}else if (sign == "^"){
+		} else if (sign == "^") {
 			int result = 1;
-			for (int i = 1; i <= secondNumber; i++){
+			for (int i = 1; i <= secondNumber; i++) {
 				result *= firstNumber;
 			}
-		}else if (sign == "%"){
+			System.out.println(result);
+		} else if (sign == "%"){
 			System.out.println(firstNumber % secondNumber);
 		}
 	}
