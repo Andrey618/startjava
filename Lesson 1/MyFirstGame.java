015efa9878c5f6a08fba93d7e561a884e714451b
Andrey618 <andrey618@gmail.com>
2018-08-05 17:39:52 +0300

    added BREAK in WHILE, deleled commented code

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,46 +1,22 @@
 public class MyFirstGame {	
 	
 	public static void main(String[] args) {
-		int compNumber = 0;
+		int compNumber = 100;
 		int userNumber = 0;
 	
 		System.out.println("Компьютер загадал число: " + compNumber);				
-		
-		// if (compNumber == userNumber) {
-		// 	System.out.println("Вы угадали с первого раза! Это число: " + userNumber);
-		// } else {
-			while (userNumber != compNumber) {
-			if (userNumber < compNumber) {
+		while (userNumber != compNumber) {
+			if (compNumber == userNumber) {
+				System.out.println("Вы угадали! Это число: " + userNumber);
+				break;
+			} else if (userNumber < compNumber) {
 				System.out.println("Введенное вами число (" + userNumber + ") меньше того, что загадал компьютер");
 				userNumber++;
 			} else if (userNumber > compNumber) {
 				System.out.println("Введенное вами число (" + userNumber + ") больше того, что загадал компьютер");
 				userNumber--;
-				}
 			}
+		}
 		System.out.println("Вы угадали! Это число: " + userNumber);	
-		// }
 	}
-}		
-
-
-		// System.out.println("Компьютер загадал число: " + compNumber);				
-		
-		// if (compNumber == userNumber) {
-		// 	System.out.println("Вы угадали! Это число: " + userNumber);
-		// } else if (userNumber < compNumber) {
-		// 	while (userNumber != compNumber) {
-		// 		System.out.println("Введенное вами число (" + userNumber + ") меньше того, что загадал компьютер");
-		// 		userNumber ++;
-		// 	}
-		// 	System.out.println("Вы угадали! Это число: " + userNumber);
-		// } else if (userNumber > compNumber) {
-		// 	while (userNumber != compNumber) {
-		// 		System.out.println("Введенное вами число (" + userNumber + ") больше того, что загадал компьютер");
-		// 		userNumber --;
-		// 	}
-		// 	System.out.println("Вы угадали! Это число: " + userNumber);	
-		// }
-			
-
-	
+}
