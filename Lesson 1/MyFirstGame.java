2c449527b8886608720b599a933f725b044e84f2
Andrey618 <andrey618@gmail.com>
2018-08-05 16:22:18 +0300

    change to one While-cycle (task from Max)

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -2,26 +2,45 @@ public class MyFirstGame {
 	
 	public static void main(String[] args) {
 		int compNumber = 65;
-		int userNumber = 50;
+		int userNumber = 0;
 	
 		System.out.println("Компьютер загадал число: " + compNumber);				
 		
 		if (compNumber == userNumber) {
-			System.out.println("Вы угадали! Это число: " + userNumber);
-		} else if (userNumber < compNumber) {
+			System.out.println("Вы угадали с первого раза! Это число: " + userNumber);
+		} else {
 			while (userNumber != compNumber) {
+			if (userNumber < compNumber) {
 				System.out.println("Введенное вами число (" + userNumber + ") меньше того, что загадал компьютер");
 				userNumber ++;
-			}
-			System.out.println("Вы угадали! Это число: " + userNumber);
-		} else if (userNumber > compNumber) {
-			while (userNumber != compNumber) {
+			} else if (userNumber > compNumber) {
 				System.out.println("Введенное вами число (" + userNumber + ") больше того, что загадал компьютер");
 				userNumber --;
+				}
 			}
-			System.out.println("Вы угадали! Это число: " + userNumber);	
+		System.out.println("Вы угадали! Это число: " + userNumber);	
 		}
-			
 	}
-}
+}		
+
+
+		// System.out.println("Компьютер загадал число: " + compNumber);				
+		
+		// if (compNumber == userNumber) {
+		// 	System.out.println("Вы угадали! Это число: " + userNumber);
+		// } else if (userNumber < compNumber) {
+		// 	while (userNumber != compNumber) {
+		// 		System.out.println("Введенное вами число (" + userNumber + ") меньше того, что загадал компьютер");
+		// 		userNumber ++;
+		// 	}
+		// 	System.out.println("Вы угадали! Это число: " + userNumber);
+		// } else if (userNumber > compNumber) {
+		// 	while (userNumber != compNumber) {
+		// 		System.out.println("Введенное вами число (" + userNumber + ") больше того, что загадал компьютер");
+		// 		userNumber --;
+		// 	}
+		// 	System.out.println("Вы угадали! Это число: " + userNumber);	
+		// }
+			
+
 	
