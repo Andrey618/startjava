b3a88b3021bb480a6d1f116e39c37a14af941e7c
Andrey618 <andrey618@gmail.com>
2018-08-07 00:09:33 +0300

    Calcularot was refactored

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -2,24 +2,24 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		int firstNumber = 17;
-		int secondNumber = 5;
-		String sign = "^";
+		int secondNumber = 0;
+		char sign = '^';
 
-		if (sign == "+") {
+		if (sign == '+') {
 			System.out.println(firstNumber + secondNumber);
-		} else if (sign == "-") {
+		} else if (sign == '-') {
 			System.out.println(firstNumber - secondNumber);
-		} else if (sign == "*") {
+		} else if (sign == '*') {
 			System.out.println(firstNumber * secondNumber);
-		} else if (sign == "/") {
+		} else if (sign == '/') {
 			System.out.println((double)firstNumber / secondNumber);
-		} else if (sign == "^") {
+		} else if (sign == '^') {
 			int result = 1;
 			for (int i = 1; i <= secondNumber; i++) {
 				result *= firstNumber;
 			}
 			System.out.println(result);
-		} else if (sign == "%") {
+		} else if (sign == '%') {
 			System.out.println(firstNumber % secondNumber);
 		}
 	}
