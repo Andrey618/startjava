a6c4fb8b77d652e4309c884976c64f429d325d2b
Andrey618 <andrey618@gmail.com>
2018-08-12 13:22:24 +0300

    Right Calculator

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,79 +1,45 @@
+import java.util.Scanner;
+
 public class Calculator {	
 	
-	private int firstNumber;
-	private int secondNumber;
-	private char sign;
-	private double resultValue;
+	public double calcalateResult() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Введите первое число: ");
+		int firstNumber = scan.nextInt();
 
-	public void setFirstNumber(int firstNumber) {
-		this.firstNumber = firstNumber;
-	// public void setFirstNumber(int fn) {	
-	// 	firstNumber = fn;
-	}
-	public void setSecondNumber(int secondNumber) {
-		this.secondNumber = secondNumber;
-	// public void setSecondNumber(int sn) {
-	// 	secondNumber = sn;
-	}
+		System.out.print("Введите знак математической операции: ");
+		char mathOperator = scan.next().charAt(0);
 
-	public void setSign(char sign) {
-		this.sign = sign;
-	// public void setSign(char si) {
-	// 	sign = si;
-	}
-
-	public double getResult() {
-		resultValue = calcalateResult(firstNumber, secondNumber, sign);
-		return resultValue;
-	}
-
-	private double calcalateResult(int firstNumber, int secondNumber, char sign) {
+		System.out.print("Введите второе число: ");
+		int secondNumber = scan.nextInt();
+		
 		double result = 0;
-		if (sign == '+') {
-			result = firstNumber + secondNumber;
-		} else if (sign == '-') {
-			result = firstNumber - secondNumber;
-		} else if (sign == '*') {
-			result = (firstNumber * secondNumber);
-		} else if (sign == '/') {
-			result = (double)firstNumber / secondNumber;
-		} else if (sign == '^') {
-			result = 1;
-			for (int i = 1; i <= secondNumber; i++) {
-				result *= firstNumber;
-			}
-		} else if (sign == '%') {
-			result = firstNumber % secondNumber;
+
+		switch(mathOperator) {
+			case '+': 
+				result = firstNumber + secondNumber;
+				break;
+			case '-': 
+				result = firstNumber - secondNumber;
+				break;
+			case '*': 
+				result = firstNumber * secondNumber;
+				break;
+			case '/': 
+				result = (double)firstNumber / secondNumber;
+				break;
+			case '^': 
+				result = 1;
+				for (int i = 1; i <= secondNumber; i++) {
+					result *= firstNumber;
+				}
+				break;
+			case '%': 
+				result = firstNumber % secondNumber;
+				break;
 		}
-		// result = 10;
+		
 		return result;
 	}
-
-
-
-
-
-	// public static void main(String[] args) {
-	// 	int firstNumber = 17;
-	// 	int secondNumber = 0;
-	// 	char sign = '^';
-
-	// 	if (sign == '+') {
-	// 		System.out.println(firstNumber + secondNumber);
-	// 	} else if (sign == '-') {
-	// 		System.out.println(firstNumber - secondNumber);
-	// 	} else if (sign == '*') {
-	// 		System.out.println(firstNumber * secondNumber);
-	// 	} else if (sign == '/') {
-	// 		System.out.println((double)firstNumber / secondNumber);
-	// 	} else if (sign == '^') {
-	// 		int result = 1;
-	// 		for (int i = 1; i <= secondNumber; i++) {
-	// 			result *= firstNumber;
-	// 		}
-	// 		System.out.println(result);
-	// 	} else if (sign == '%') {
-	// 		System.out.println(firstNumber % secondNumber);
-	// 	}
-	// }
 }
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -3,15 +3,6 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		
 		Calculator calc = new Calculator ();
-		calc.setFirstNumber(17);
-		calc.setSecondNumber(5);
-		calc.setSign('+');
-
-		System.out.println("Test: is main class is working?");
-
-		System.out.println(calc.getResult());
-
-		//System.out.println(firstNumber % secondNumber);
-	
+		System.out.println(calc.calcalateResult());
 	}
 }
