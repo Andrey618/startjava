4f85296d6c788bfec77d810a32f918b35773e3f1
Andrey618 <andrey618@gmail.com>
2018-08-14 00:00:45 +0300

    Does not read да/нет rightly

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -3,34 +3,36 @@ import java.util.Scanner;
 public class CalculatorTest {	
 	
 	public static void main(String[] args) {
+		String response = "да";
 
 		Calculator calc = new Calculator();
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Введите первое число: ");
-		calc.firstNumber = scan.nextInt();
+		while(response.equals("да")) {
+			            	
+			System.out.print("Введите первое число: ");
+			calc.firstNumber = scan.nextInt();
 
-		System.out.print("Введите знак математической операции: ");
-		calc.mathOperator = scan.next().charAt(0);
+			System.out.print("Введите знак математической операции: ");
+			calc.mathOperator = scan.next().charAt(0);
 
-		System.out.print("Введите второе число: ");
-		calc.secondNumber = scan.nextInt();
+			System.out.print("Введите второе число: ");
+			calc.secondNumber = scan.nextInt();
 
-		System.out.println(calc.calculateResult());
-		
-		String response = null;
-
-		while(response != "да" | response != "нет") {
-			System.out.println("Хотите продолжить? [да/нет]: ");
-			response = scan.next();
-			System.out.println("Вы ввели: " + response);
-			if (response == "да") {
-            	System.out.println("вы выбрали да, продолжим ...");
-      		} else if (response == "нет") {
-				System.out.println("вы выбрали нет, пока!");
-			} else {
-				System.out.println("вы выбрали неправильный вариант");
-			}
+			System.out.println("Получилось: " + calc.calculateResult());
+			System.out.println();
+			response = "что-то еще";
+			while ((!response.equals("да")) && (!response.equals("нет"))){
+				System.out.println("Хотите продолжить? [да/нет]: ");
+				response = scan.next();
+				// System.out.println("Вы ввели: " + response);
+				if (response.equals("нет")) {
+					System.out.println("Удачи!");
+					break;	
+				} else if (response.equals("да")) {
+					break;
+				} else System.out.println(response + " - это неправильный вариант");				}
+			
 		}
 	}
 }
