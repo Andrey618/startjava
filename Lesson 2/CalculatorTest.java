c61732d2cff97e6d2f42375b48f3fb60b9184a88
Andrey618 <andrey618@gmail.com>
2018-08-14 22:25:44 +0300

    4 not needed stings

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -9,7 +9,6 @@ public class CalculatorTest {
 		Scanner scan = new Scanner(System.in);
 		
 		while(response.equals("да")) {
-			            	
 			System.out.print("Введите первое число: ");
 			calc.setFirstNumber(scan.nextInt());
 
@@ -19,17 +18,17 @@ public class CalculatorTest {
 			System.out.print("Введите второе число: ");
 			calc.setSecondNumber(scan.nextInt());
 
-			System.out.println("Получилось: " + calc.calculateResult());
-			System.out.println();
+			System.out.println("Получилось: " + calc.calculateResult() + "\n");
 			
 			while ((!response.equals("да")) || (!response.equals("нет"))) {
-				System.out.println("Хотите продолжить? [да/нет]: ");
+				System.out.print("Хотите продолжить? [да/нет]: ");
 				response = scan.next();
 				if (response.equals("нет")) {
 					break;	
 				} else if (response.equals("да")) {
 					break;
-				} else System.out.println(response + " - это неправильный вариант");				
+				}
+				 else System.out.println(response + " - это неправильный вариант \n");				
 			}
 		}
 		System.out.println("Удачи!");
