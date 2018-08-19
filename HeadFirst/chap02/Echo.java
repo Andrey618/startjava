48390625199c680fe5c825d45b5687c48fa73fbb
Andrey618 <andrey618@gmail.com>
2018-08-19 15:57:51 +0300

    Head first

---
+++ HeadFirst/chap02/Echo.java
@@ -0,0 +1,6 @@
+class Echo { // added "Echo"
+    int count = 0; // added "count" 
+    void hello() { // added "hello"
+        System.out.println("helloooo...");
+    }
+}
---
+++ HeadFirst/chap02/GameLauncher.java
@@ -0,0 +1,8 @@
+// package chap02; //!!! There is som problem with this package
+
+public class GameLauncher {
+    public static void main (String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
