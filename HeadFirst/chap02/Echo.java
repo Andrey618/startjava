66b468ff83c467054ac2baa7ec8d6aeec7f3940d
Andrey618 <andrey618@gmail.com>
2018-08-29 08:33:28 +0300

    Remove HeadFirst directory

--- HeadFirst/chap02/Echo.java
+++
@@ -1,6 +0,0 @@
-class Echo { // added "Echo"
-    int count = 0; // added "count" 
-    void hello() { // added "hello"
-        System.out.println("helloooo...");
-    }
-}
--- HeadFirst/chap02/GameLauncher.java
+++
@@ -1,8 +0,0 @@
-// package chap02; //!!! There is som problem with this package
-
-public class GameLauncher {
-    public static void main (String[] args) {
-        GuessGame game = new GuessGame();
-        game.startGame();
-    }
-}
